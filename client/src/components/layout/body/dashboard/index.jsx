9bda2100256906d45b9710de7c10faf333b13bd6
Amine TABOU <amin.tabou@gmail.com>
2023-02-22 19:01:36 +0100

    ajout new features

--- client/src/components/layout/body/dashboard/index.jsx
+++ client/src/components/layout/body/dashboard/index.jsx
@@ -3,20 +3,10 @@ import { Row, Col } from 'antd';
 import PageTitle from './PageTitle';
 import Content from './content';
 import GoTop from './GoTop';
-import PieChartItem from './content/main/map/PieChartItem';
 
-const data = [
-  { name: 'Group A', value: 400 }, // 33
-  { name: 'Group B', value: 300 }, // 25
-  { name: 'Group C', value: 300 }, // 25
-  { name: 'Group D', value: 200 }, // 17
-];
 function Dashboard() {
   return (
     <Row className="dashboard">
-      <Col span={24}>
-        <PieChartItem data={data} />
-      </Col>
       <Col span={24}>
         <PageTitle />
       </Col>
